dc8ff36fb042f47f6296de4b8d2daf4f30807741
JChr15t <yousefaaaammar.com>
2024-08-12 12:13:28 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::apiResource('/petitions', PetitionController::class);
-
-Route::resource('/authors', AuthController::class)
-->only(['index', 'show']);
